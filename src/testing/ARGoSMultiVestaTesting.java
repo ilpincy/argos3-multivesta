3305ad2759ddbf71699496963d360c11e758fa7a
Andrea Vandin <andrea.vandin@imtlucca.it>
2013-09-24 15:09:57 +0200

    prova

--- src/testing/ARGoSMultiVestaTesting.java
+++ src/testing/ARGoSMultiVestaTesting.java
@@ -6,10 +6,17 @@ import vesta.mc.ParametersForState;
 public class ARGoSMultiVestaTesting {
 	public static void main(String[] args) {
 		String fileWithModelSpecification = "../test_footbot_lua.xml";
-		String otherParameters = "libraryPath=argos3_multivesta";
+		//String otherParameters = "libraryPath=argos3_multivesta";
+    //String otherParameters = "libraryPath=libargos3_multivesta";
+    //String otherParameters = "libraryPath=/home/andrea/Dropbox/prova/argos3-multivesta/build/libargos3_multivesta";
+    String otherParameters = "libraryPath=";
+    if(args.length != 0)
+      otherParameters += args[0];
+    else  
+      otherParameters += "libargos3_multivesta";
 		ParametersForState params = new ParametersForState(
 				fileWithModelSpecification, otherParameters);
 		ARGoSState a = new ARGoSState(params);
-		a.performWholeSimulation();
+		a.performWholeSimulation();	
 	}
 }
