995fa820214c6e84ded870394e9beedcce12b1a3
Andrea Vandin <andrea.vandin@imtlucca.it>
2013-09-24 15:19:15 +0200

    adding missing bracket

--- src/testing/ARGoSMultiVestaTesting.java
+++ src/testing/ARGoSMultiVestaTesting.java
@@ -6,18 +6,18 @@ import vesta.mc.ParametersForState;
 public class ARGoSMultiVestaTesting {
 
 	public static void main(String[] args) {
-      String configFile = "../src/testing/diffusion_10.argos";
+		String configFile = "../src/testing/diffusion_10.argos";
 		//String otherParameters = "libraryPath=argos3_multivesta";
-    //String otherParameters = "libraryPath=libargos3_multivesta";
-    //String otherParameters = "libraryPath=/home/andrea/Dropbox/prova/argos3-multivesta/build/libargos3_multivesta";
-    String otherParameters = "libraryPath=";
-    if(args.length != 0)
-      otherParameters += args[0];
-    else  
-      otherParameters += "libargos3_multivesta";
-		ParametersForState params = new ParametersForState(
-				configFile, otherParameters);
+		//String otherParameters = "libraryPath=libargos3_multivesta";
+		//String otherParameters = "libraryPath=/home/andrea/Dropbox/prova/argos3-multivesta/build/libargos3_multivesta";
+		String otherParameters = "libraryPath=";
+		if(args.length != 0)
+			otherParameters += args[0];
+		else  
+			otherParameters += "libargos3_multivesta";
+		ParametersForState params = new ParametersForState(configFile, otherParameters);
 		ARGoSState a = new ARGoSState(params);
 		a.performWholeSimulation();	
 	}
+}	
 
